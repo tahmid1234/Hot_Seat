d32e819a0f6a50682317f1e2033908ae2ea12fbc
tahmid1234 <tahmidislam1998@gmail.com>
2021-11-20 10:25:15 +0600

    Project Created

---
+++ Hot_Seat_Application/app/src/main/java/com/example/hot_sear/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hot_sear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
